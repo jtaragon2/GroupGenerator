44a3683f510004320f81d9721ef204ce77a4a84b
jtara <jtara@10.0.0.174>
2022-04-12 17:00:30 -0600

    Completed the first round of groups. Will always add to three groups without skipping, even if the number of students is not divisible by three.

--- GroupGenerator/src/Main.java
+++ GroupGenerator/src/Main.java
@@ -1,27 +1,44 @@
 import java.util.*;
 import java.io.*;
+
 public class Main
 	{
-
-
-		public static void main(String[] args) throws IOException
-					{
-						readTxt();
-					}
 		
-		public static void readTxt() throws IOException
-		{
-						Scanner myFile = new Scanner(new File("textFileGroups.txt"));
+		static ArrayList<Student> studentList = new ArrayList<Student>();
+		
+		public static void main(String[] args) throws IOException
+			{
+				readTxt("textFileGroups.txt");
+				GroupLists.addToGroups(studentList);
+				printLists(GroupLists.Group1);
+				printLists(GroupLists.Group2);
+				printLists(GroupLists.Group3);
+			}
+			
+		public static void readTxt(String s) throws IOException
+			{
+				Scanner myFile = new Scanner(new File(s));
+				
+				while (myFile.hasNext())
+					{
+						String line = myFile.nextLine();
 						
-						while(myFile.hasNext())
+						String[] splitArray = line.split(" ");
+						
+						for (int i = 0; i < splitArray.length - 1; i += 2)
 							{
-								String line = myFile.nextLine();
-								
-						String [] splitArray = line.split(" ");
-									
-								//add each to arraylist
+								studentList.add(new Student(splitArray[i], splitArray[i + 1]));
 							}
-
-		}
-
-}
+					}
+					
+			}
+			
+		public static void printLists(List<Student> l)
+			{
+				System.out.println("\nGroup Size: "+l.size());
+				for (Student st : l)
+					{
+						System.out.println(st.getLastName() + ", " + st.getFirstName());
+					}
+			}
+	}
