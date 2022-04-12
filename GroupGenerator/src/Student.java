66b297bc5da2f8f45b246cd3df48c4b0c4eec979
jonathan.aragon <jonathan.aragon@MU-C1-15.mullen.pvt.k12.co.us>
2022-04-12 13:45:45 -0600

    Formatted Students

--- GroupGenerator/src/Student.java
+++ GroupGenerator/src/Student.java
@@ -4,27 +4,36 @@ public class Student
 		private String firstName;
 		private String lastName;
 		int[] groups = new int[3];
-		public Student(String fName, String lName) {
-			firstName = fName;
-			lastName = lName;
-		}
+
+		public Student(String fName, String lName)
+			{
+				firstName = fName;
+				lastName = lName;
+			}
+
 		public String getFirstName()
 			{
 				return firstName;
 			}
-		
+
 		public String getLastName()
 			{
 				return lastName;
 			}
-		public int firstGroup() {
-			return groups[0];
-		}
-		public int secondGroup() {
-			return groups[1];
-		}
-		public int thirdGroup() {
-			return groups[2];
-		}
+
+		public int firstGroup()
+			{
+				return groups[0];
+			}
+
+		public int secondGroup()
+			{
+				return groups[1];
+			}
+
+		public int thirdGroup()
+			{
+				return groups[2];
+			}
 
 	}
