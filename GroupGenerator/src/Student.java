435b12369c3c91ff4c32318f790c1e2fc5bd8680
jonathan.aragon <jonathan.aragon@MU-C1-15.mullen.pvt.k12.co.us>
2022-04-12 13:29:56 -0600

    Added Students

---
+++ GroupGenerator/src/Student.java
@@ -0,0 +1,22 @@
+
+public class Student
+	{
+		private String firstName;
+		private String lastName;
+		int[] groups = new int[3];
+		public Student(String fName, String lName) {
+			firstName = fName;
+			lastName = lName;
+		}
+		public String getFirstName()
+			{
+				return firstName;
+			}
+		
+		public String getLastName()
+			{
+				return lastName;
+			}
+		
+
+	}
