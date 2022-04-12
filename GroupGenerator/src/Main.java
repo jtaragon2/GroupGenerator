5abd0cc5de367931f3dd1111e4252ec98b4f2825
carter.hill <carter.hill@MU-C1-05.mullen.pvt.k12.co.us>
2022-04-12 13:40:50 -0600

    add read

--- GroupGenerator/src/Main.java
+++ GroupGenerator/src/Main.java
@@ -3,10 +3,24 @@ import java.io.*;
 public class Main
 	{
 
-		public static void main(String[] args)
-			{
-				// TODO Auto-generated method stub
-
+		public static void main(String[] args) throws IOException
+					{
+						readTxt();
+					}
+		
+		public static void readTxt() throws IOException
+		{
+						Scanner myFile = new Scanner(new File("textFileGroups.txt"));
+						
+						while(myFile.hasNext())
+							{
+								String line = myFile.nextLine();
+								
+						String [] splitArray = line.split(" ");
+									
+								//add each to arraylist
+							}
 			}
+		}
 
-	}
+	
